a10d1d0277ca10784bc16bfb4df193449ed9e042
JOSEPH <fatomijoseph1469@gmail.com>
2023-05-18 21:23:02 +0100

    first commit

---
+++ JS/main.js
@@ -0,0 +1,28 @@
+
+function toggle(){
+    let icon = document.getElementById("whatsapp-logo")
+    if (icon.style.display === "none"){
+        icon.style.display = "inline"
+    }else{
+        icon.style.display = "none"
+    }
+    let icon = document.getElementById()
+}
+function toggles(){
+    let icon = document.getElementById("currency")
+    if (icon.style.display === "none"){
+        icon.style.display = "block"
+    }else{
+        icon.style.display = "none"
+    }
+    
+}
+function appear(){
+    let icon = document.getElementById("access")
+    if (icon.style.display === "none"){
+        icon.style.display = "block"
+    }else{
+        icon.style.display = "none"
+    }
+    
+}
