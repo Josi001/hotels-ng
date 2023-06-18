84e60000ef90221e0efa900d5acff090d4a31f4b
JOSEPH <fatomijoseph1469@gmail.com>
2023-06-18 17:45:34 +0100

    BUGFIXES-(last-part)

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,8 @@ function toggle(){
     let icon = document.getElementById("woman-dropdown-menu")
     if (icon.style.display === "none"){
         icon.style.display = "inline"
+        document.getElementById("currency-dropdown-menu").style.display = "none"
+        document.getElementById("acct-dropdown-menu").style.display = "none"
     }else{
         icon.style.display = "none"
     }
@@ -15,6 +17,8 @@ function toggles(){
     let icon = document.getElementById("currency-dropdown-menu")
     if (icon.style.display === "none"){
         icon.style.display = "inline"
+        document.getElementById("woman-dropdown-menu").style.display = "none"
+        document.getElementById("acct-dropdown-menu").style.display = "none"
     }else{
         icon.style.display = "none"
     }
@@ -27,6 +31,8 @@ function appear(){
     let icon = document.getElementById("acct-dropdown-menu")
     if (icon.style.display === "none"){
         icon.style.display = "inline"
+        document.getElementById("woman-dropdown-menu").style.display = "none"
+        document.getElementById("currency-dropdown-menu").style.display = "none"
     }else{
         icon.style.display = "none"
     }
